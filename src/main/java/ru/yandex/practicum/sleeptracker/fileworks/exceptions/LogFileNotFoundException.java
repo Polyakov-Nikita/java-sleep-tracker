4dff471d8410a595aefe8117613106a42e4f2db9
Nikita Polyakov <poliak0vn@yandex.ru>
2026-01-21 12:59:24 +0300

    style correction v2.1.2

--- src/main/java/ru/yandex/practicum/sleeptracker/fileworks/exceptions/LogFileNotFoundException.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/fileworks/exceptions/LogFileNotFoundException.java
@@ -3,10 +3,10 @@ package ru.yandex.practicum.sleeptracker.fileworks.exceptions;
 import java.nio.file.Path;
 
 public class LogFileNotFoundException extends RuntimeException {
-    public final Path LogFile;
+    public final Path logFile;
 
     public LogFileNotFoundException(Path logFile) {
         super("Файл лога не найден.");
-        LogFile = logFile;
+        this.logFile = logFile;
     }
 }
